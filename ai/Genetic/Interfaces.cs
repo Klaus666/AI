f6476bc52945eee634a551868598a2223a6bc152
Klaus666 <cursing@mail.ru>
2016-01-25 14:51:27 +1000

    Genetic Algotithm

---
+++ ai/Genetic/Interfaces.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic
+{
+    public interface IChromosome<C> where C : IChromosome<C>
+    {
+        C[] Crossover(C anotherChromosome);
+
+        C Mutate();
+    }
+
+    public interface Fitness<C, T>
+        where C : IChromosome<C>
+        where T : IComparable<T>
+    {
+        T Calculate(C chromosome);
+    }
+
+    public interface IIterationListener<C, T>
+        where C : IChromosome<C>
+        where T : IComparable<T>
+    {
+        void Update(GeneticAlgorithm<C, T> environment);
+    }
+}
