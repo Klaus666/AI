2244d8d7a79b8d23d7c34568f27cb4364b7439c4
DiligentKeyPresser <tkachev.daniil@gmail.com>
2016-01-25 11:12:23 +1000

    env

--- ai/MultiagentEnvironment/evolution/EatenFoodObserver.cs
+++ ai/MultiagentEnvironment/evolution/EatenFoodObserver.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MultiagentEnvironment.evolution
+namespace MultiagentEnvironment
 {
     public class EatenFoodObserver : AgentsEnvironmentObserver
     {
